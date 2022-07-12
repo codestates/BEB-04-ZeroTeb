4291081c2f9df58ee2953dd1ecd85830ac929d27
이지민 <jm@ijimin-ui-Macmini.local>
2022-07-12 09:25:59 +0900

    Feat [server start ms], [token controller, service base]

--- server/src/token/token.controller.ts
+++ server/src/token/token.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { UpdateTokenDto } from './dto/update-token.dto';
@@ -7,28 +7,37 @@ import { UpdateTokenDto } from './dto/update-token.dto';
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
-  @Post()
-  create(@Body() createTokenDto: CreateTokenDto) {
-    return this.tokenService.create(createTokenDto);
+  @Get('list')
+  findTokenList(@Query() address: string): object {
+    return this.tokenService.findTokenList(address);
+  }
+  @Get('qrcode')
+  createTokenQR(@Query('address') address: string, @Query('token_id') token_id: string) {
+    return this.tokenService.createTokenQR(address, token_id);
   }
 
-  @Get()
-  findAll() {
-    return this.tokenService.findAll();
+  @Get('qrcode/validation')
+  checkValidation(@Body() nonce: string) {
+    return this.tokenService.checkValidation(nonce);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tokenService.findOne(+id);
-  }
+  // @Post()
+  // create(@Body() createTokenDto: CreateTokenDto) {
+  //   return this.tokenService.create(createTokenDto);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTokenDto: UpdateTokenDto) {
-    return this.tokenService.update(+id, updateTokenDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.tokenService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tokenService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTokenDto: UpdateTokenDto) {
+  //   return this.tokenService.update(+id, updateTokenDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.tokenService.remove(+id);
+  // }
 }
--- server/src/token/token.service.ts
+++ server/src/token/token.service.ts
@@ -4,23 +4,48 @@ import { UpdateTokenDto } from './dto/update-token.dto';
 
 @Injectable()
 export class TokenService {
-  create(createTokenDto: CreateTokenDto) {
-    return 'This action adds a new token';
+  findTokenList(address: string): object {
+    //주소로 DB에 데이터 호출
+
+    //반환할 토큰 데이터
+    const tokenList = {
+      token_id: '123456',
+      token_image_url: 'http://sdts.dsf',
+    };
+    return tokenList;
+  }
+  createTokenQR(address: string, token_id: string) {
+    //address와 token_id로 qrcode 생성
+
+    return 'qrcode';
+  }
+  checkValidation(nonce: string) {
+    //nonce가 유효한지 확인
+
+    const nonceValidation = {
+      message: '유효성 검사 결과',
+      nonce: '필요한가? nonce',
+    };
+    return nonceValidation;
   }
 
-  findAll() {
-    return `This action returns all token`;
-  }
+  // create(createTokenDto: CreateTokenDto) {
+  //   return 'This action adds a new token';
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} token`;
-  }
+  // findAll() {
+  //   return `This action returns all token`;
+  // }
 
-  update(id: number, updateTokenDto: UpdateTokenDto) {
-    return `This action updates a #${id} token`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} token`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} token`;
-  }
+  // update(id: number, updateTokenDto: UpdateTokenDto) {
+  //   return `This action updates a #${id} token`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} token`;
+  // }
 }
