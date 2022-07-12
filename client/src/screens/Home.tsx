0b2a9866da5105b4e33f37e48ff1cb5515e65f84
cochae <100351462+chee9835@users.noreply.github.com>
2022-07-12 09:16:44 +0900

    Feat: bottomtabnavigator

---
+++ client/src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  )
+}
+
+export default Home
