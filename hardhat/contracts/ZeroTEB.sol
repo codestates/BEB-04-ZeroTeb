ae7d4eeb22de485171c50691e683d738c4b4c973
BeeImp <zero9231@naver.com>
2022-07-19 21:01:37 +0900

    Refactor: 컨트랙트 수정 및 통합

--- hardhat/contracts/ZeroTEB.sol
+++ hardhat/contracts/ZeroTEB.sol
@@ -1,167 +1,297 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import "./IZeroTEB.sol";
-// import "@klaytn/contracts/access/Ownable.sol";
-// import "@klaytn/contracts/utils/Counters.sol";
+import "./IZeroTEB.sol";
+import "@klaytn/contracts/access/Ownable.sol";
+import "@klaytn/contracts/utils/Counters.sol";
+import "@klaytn/contracts/KIP/token/KIP17/KIP17.sol";
+import "@klaytn/contracts/KIP/token/KIP17/extensions/KIP17URIStorage.sol";
 
-// contract ZeroTEB is IZeroTEB, Ownable {
-//     using Counters for Counters.Counter;
-//     Counters.Counter private _eventIds;
-//     address public _nftContractAddress;
-//     // address public _sbtContractAddress;
+contract ZeroTEB is IZeroTEB, Ownable, KIP17URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _eventIds;
+    Counters.Counter private _tokenIds;
 
-//     struct Event {
-//         string name;
-//         uint256 price;
-//         string tokenImageUri;
-//         address creator;
-//         uint256 openTime;
-//         uint256 closeTime;
-//         uint256 endTime;
-//         uint256[] tokens;
-//         uint256 count;
-//     }
+    // 이벤트 구조체
+    struct EventClass {
+        string name;
+        uint256 price;
+        uint256 count;
+        uint256[] tokens;
+        address[] owners;
+    }
 
-//     mapping(uint => Event) _events;
+    struct Event {
+        string name;
+        string tokenImageUri;
+        address creator;
+        uint256 classCount;
+        uint256 openTime;
+        uint256 closeTime;
+        uint256 endTime;
+        mapping(uint8 => EventClass) class;
+    }
 
-//     constructor(address nftContractAddress_) {
-//         _nftContractAddress = nftContractAddress_;
-//         // _sbtContractAddress = sbtContractAddress_;
-//     }
+    // 토큰 확장 구조체
+    struct TokenExtention {
+        uint256 eventId;
+        bool isTrade;
+        uint8 tokenType;
+    }
 
-//     function getEvent(uint _eventId) public view returns (Event memory) {
-//         return _events[_eventId];
-//     }
+    /*
+     * Mapping
+     */
+    mapping(uint256 => Event) _events;
+    mapping(uint256 => TokenExtention) private _tokenExtentions;
+    mapping(uint256 => address[]) _eventParticipants; // 이벤트 응모 참가자
 
-//     function createEvent(
-//         address _creator,
-//         string memory _eventName,
-//         uint256 _eventPrice,
-//         string memory _tokenImageUri,
-//         uint8 _count,
-//         uint256 _openTime,
-//         uint256 _closeTime,
-//         uint256 _endTime
-//     ) public override onlyOwner returns (uint256 _eventId) {
-//         // 이벤트 아이디
-//         uint256 _newEventId = _eventIds.current();
+    /*
+     * Modifier
+     */
+    modifier isEndEvent(uint256 _eventId) {
+        require(
+            block.timestamp < _events[_eventId].endTime,
+            "This Event is not close."
+        );
+        _;
+    }
+    modifier isNFT(uint256 _tokenId) {
+        require(
+            _tokenExtentions[_tokenId].tokenType == 0,
+            "This Token is not NFT."
+        );
+        _;
+    }
 
-//         // 신규 이벤트 매핑
-//         _events[_newEventId] = Event({
-//             name: _eventName,
-//             price: _eventPrice,
-//             tokenImageUri: _tokenImageUri,
-//             creator: _creator,
-//             openTime: _openTime,
-//             closeTime: _closeTime,
-//             endTime: _endTime,
-//             tokens: new uint[](_count),
-//             count: 0
-//         });
+    constructor(string memory _name, string memory _symbol)
+        KIP17(_name, _symbol)
+    {}
 
-//         // 이벤트 아이디 증가
-//         _eventIds.increment();
+    // function getEvent(uint _eventId) public view returns (Event memory) {
+    //     return _events[_eventId];
+    // }
 
-//         // 반환
-//         _eventId = _newEventId;
-//     }
+    function eventOf(uint _tokenId) public view returns (uint) {
+        return _tokenExtentions[_tokenId].eventId;
+    }
 
-//     // Test
-//     function setNftContractAddress(address _contract) public returns (address) {
-//         _nftContractAddress = _contract;
-//         return _nftContractAddress;
-//     }
+    function createEvent(
+        address _creator,
+        string memory _eventName,
+        string memory _tokenImageUri,
+        string[] memory _names,
+        uint256[] memory _prices,
+        uint256[] memory _counts,
+        uint256 _openTime,
+        uint256 _closeTime,
+        uint256 _endTime
+    ) public override onlyOwner returns (uint256 _eventId) {
+        // 이벤트 아이디
+        uint256 _newEventId = _eventIds.current();
 
-//     function test(string memory _string) public returns (string memory) {
-//         (bool result, bytes memory data) = _nftContractAddress.call(
-//             abi.encodeWithSignature("test(string)", _string)
-//         );
-//         require(result, "error");
-//         string memory _result = abi.decode(data, (string));
-//         return _result;
-//     }
+        // 신규 이벤트 매핑
+        _events[_newEventId].name = _eventName;
+        _events[_newEventId].tokenImageUri = _tokenImageUri;
+        _events[_newEventId].creator = _creator;
+        _events[_newEventId].openTime = _openTime;
+        _events[_newEventId].closeTime = _closeTime;
+        _events[_newEventId].endTime = _endTime;
+        _events[_newEventId].classCount = _names.length;
 
-//     function mintEvent(
-//         uint256 _eventId,
-//         uint8 _tokenType, // 0 : NFT, 1: SBT
-//         string memory _tokenUri
-//     ) public override returns (bool) {
-//         require(_tokenType < 2, "tokenType is not defined");
-//         require(_eventId < _eventIds.current(), "EventId is not defined");
-//         bool _result = false;
-//         bytes memory data;
-//         // if (_tokenType == 0) {
-//         (_result, data) = payable(_nftContractAddress).call(
-//             abi.encodeWithSignature(
-//                 "mintNFT(address, string, uint256, uint256, uint256, uint256)",
-//                 _events[_eventId].creator,
-//                 _tokenUri,
-//                 _events[_eventId].price,
-//                 _events[_eventId].openTime,
-//                 _events[_eventId].closeTime,
-//                 _events[_eventId].endTime
-//             )
-//         );
-//         // } else if (_tokenType == 1) {
-//         //     (_result, data) = _sbtContractAddress.call(
-//         //         abi.encodeWithSignature(
-//         //             "mintSBT(address, string)",
-//         //             _events[_eventId].creator,
-//         //             _tokenUri
-//         //         )
-//         //     );
-//         // } else {
-//         //     return false;
-//         // }
-//         require(_result, "Call function failed");
+        for (uint8 i = 0; i < _names.length; i++) {
+            _events[_newEventId].class[i].name = _names[i];
+            _events[_newEventId].class[i].count = _counts[i];
+            _events[_newEventId].class[i].price = _prices[i];
+            _events[_newEventId].class[i].tokens = new uint256[](_names.length);
+            _events[_newEventId].class[i].owners = new address[](_names.length);
+        }
 
-//         uint256 _tokenId = abi.decode(data, (uint256));
+        // 이벤트 아이디 증가
+        _eventIds.increment();
 
-//         _events[_eventId].tokens[_events[_eventId].count] = _tokenId;
-//         _events[_eventId].count += 1;
+        // 반환
+        _eventId = _newEventId;
+    }
 
-//         return true;
-//     }
+    function mintToken(
+        uint256 _eventId,
+        uint8 _tokenType, // 0 : NFT, 1: SBT
+        uint8 _classId,
+        string memory _tokenUri
+    ) public override onlyOwner returns (uint256) {
+        for (uint8 i = 0; i < _events[_eventId].class[_classId].count; i++) {
+            _tokenIds.increment();
 
-//     function buyToken(uint256 _eventId, uint8 _number) public payable override {
-//         (bool _result, ) = payable(_nftContractAddress).call(
-//             abi.encodeWithSignature(
-//                 "buyToken(uint8, address)",
-//                 _events[_eventId].tokens[_number],
-//                 msg.sender
-//             )
-//         );
-//         require(_result, "buyToken is error");
-//     }
+            uint256 _newTokenId = _tokenIds.current();
+            _safeMint(msg.sender, _newTokenId);
+            _setTokenURI(_newTokenId, _tokenUri);
 
-//     function getEventBuyers(uint256 _eventId)
-//         public
-//         override
-//         returns (address[] memory)
-//     {}
+            _events[_eventId].class[_classId].tokens[i] = _newTokenId;
 
-//     function applyToken(
-//         uint256 _eventId,
-//         uint256 _tokenId,
-//         uint _openTime,
-//         uint _endTime
-//     ) public payable override {}
+            _tokenExtentions[_newTokenId] = TokenExtention({
+                eventId: _eventId,
+                tokenType: _tokenType,
+                isTrade: true
+            });
+        }
 
-//     function getEventParticipants(uint256 _eventId)
-//         public
-//         override
-//         returns (address[] memory)
-//     {}
+        return _eventId;
+    }
 
-//     function approveEventWinner(uint256 _eventId)
-//         public
-//         override
-//         returns (address[] memory)
-//     {}
+    function buyToken(
+        uint256 _eventId,
+        uint8 _classId,
+        uint8 _number
+    ) public payable override {
+        uint256 _tokenId = _events[_eventId].class[_classId].tokens[_number];
+        require(
+            _tokenExtentions[_tokenId].isTrade,
+            "This token is not traded."
+        );
+        require(
+            _events[_eventId].class[_classId].price == msg.value,
+            "This amount is not the same."
+        );
+        require(
+            _events[_eventId].openTime < block.timestamp,
+            "This time is not open."
+        );
+        require(
+            block.timestamp < _events[_eventId].closeTime,
+            "The time is close."
+        );
+        address _owner = owner();
+        _transfer(_owner, msg.sender, _tokenId);
+        _events[_eventId].class[_classId].owners[_number] = msg.sender;
+        _tokenExtentions[_tokenId].isTrade = false;
+    }
 
-//     function endEvent(uint256 _eventId, uint8 _eventEndStatus)
-//         public
-//         override
-//     {}
-// }
+    function getEventBuyers(uint256 _eventId, uint8 _classId)
+        public
+        view
+        override
+        returns (address[] memory)
+    {
+        return _events[_eventId].class[_classId].owners;
+    }
+
+    function applyToken(uint256 _eventId, uint8 _classId)
+        public
+        payable
+        override
+    {
+        require(
+            _events[_eventId].class[_classId].price == msg.value,
+            "This amount is not the same."
+        );
+        require(
+            _events[_eventId].openTime < block.timestamp,
+            "This time is not open."
+        );
+        require(
+            block.timestamp < _events[_eventId].closeTime,
+            "The time is close."
+        );
+        _eventParticipants[_eventId].push(msg.sender);
+    }
+
+    function getEventParticipants(uint256 _eventId)
+        public
+        view
+        override
+        returns (address[] memory)
+    {
+        return _eventParticipants[_eventId];
+    }
+
+    function _eventWinner() private returns (address[] memory) {}
+
+    function approveEventWinner(uint256 _eventId)
+        public
+        override
+        returns (address[] memory)
+    {}
+
+    function endEvent(uint256 _eventId, uint8 _eventEndStatus)
+        public
+        override
+    {}
+
+    /**
+     * @dev See {IKIP17-transferFrom}.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override isEndEvent(tokenId) isNFT(tokenId) {
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "KIP17: transfer caller is not owner nor approved"
+        );
+
+        _transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev See {IKIP17-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override isEndEvent(tokenId) isNFT(tokenId) {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    /**
+     * @dev See {IKIP17-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public virtual override isEndEvent(tokenId) isNFT(tokenId) {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "KIP17: transfer caller is not owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the KIP17/ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * `_data` is additional data, it has no specified format and it is sent in call to `to`.
+     *
+     * This internal function is equivalent to {safeTransferFrom}, and can be used to e.g.
+     * implement alternative mechanisms to perform token transfer, such as signature-based.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If `to` refers to a smart contract, it must implement {IKIP17Receiver-onKIP17Received}
+     *   OR {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _safeTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) internal virtual override isEndEvent(tokenId) isNFT(tokenId) {
+        _transfer(from, to, tokenId);
+        // require(
+        //     _checkOnKIP17Received(from, to, tokenId, _data) ||
+        //         _checkOnERC721Received(from, to, tokenId, _data),
+        //     "KIP17: transfer to non IKIP17Receiver/IERC721Receiver implementer"
+        // );
+    }
+}
