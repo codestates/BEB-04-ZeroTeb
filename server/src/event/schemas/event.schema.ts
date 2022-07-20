da970c2695f8162efcf97204af5c8b9d65b10360
이지민 <jm@ijimin-ui-Macmini.local>
2022-07-20 22:57:09 +0900

    Fix [date data]

--- server/src/event/schemas/event.schema.ts
+++ server/src/event/schemas/event.schema.ts
@@ -18,12 +18,12 @@ export const EventSchema = new mongoose.Schema({
   price: { type: [], required: true },
   contents: { type: String, required: true },
   option: { type: [] },
-  recruit_start_date: { type: String, default: start_date },
-  recruit_end_date: { type: String, default: end_date },
-  event_start_date: { type: String, default: start_date },
-  event_end_date: { type: String, default: end_date },
-  created_date: { type: String, default: start_date },
-  modified_date: { type: String, default: start_date },
+  recruit_start_date: { type: Number, default: start_date },
+  recruit_end_date: { type: Number, default: end_date },
+  event_start_date: { type: Number, default: start_date },
+  event_end_date: { type: Number, default: end_date },
+  created_date: { type: Number, default: start_date },
+  modified_date: { type: Number, default: start_date },
   x: { type: Number, required: true },
   y: { type: Number, required: true },
   status: { type: String, required: true },
@@ -44,12 +44,12 @@ export interface Event {
   price: [];
   contents: string;
   option: [];
-  recruit_start_date: string;
-  recruit_end_date: string;
-  event_start_date: string;
-  event_end_date: string;
-  created_date: string;
-  modified_date: string;
+  recruit_start_date: number;
+  recruit_end_date: number;
+  event_start_date: number;
+  event_end_date: number;
+  created_date: number;
+  modified_date: number;
   x: number;
   y: number;
   status: string;
