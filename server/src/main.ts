f13b14e127c558e9443b4e34f6aeae09a2e6ffdb
BeeImp <zero9231@naver.com>
2022-07-21 16:42:51 +0900

    Feat: cookieParser 추가 및 연결

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,7 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     credentials: true,
   });
-
+  app.use(cookieParser());
   await app.listen(8080, () => {
     console.log('Server Start!');
   });
