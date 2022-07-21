05c3cbb4ba28b76c2515f818edde32de0e15c284
BeeImp <zero9231@naver.com>
2022-07-21 13:36:22 +0900

    Feat: 피냐타 업로드 기능 추가

---
+++ server/lib/pinata.ts
@@ -0,0 +1,58 @@
+import Axios from 'axios';
+import FormData from 'form-data';
+
+const ipfsImageUpload = async (file, metadata) => {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('pinataOptions', '{"cidVersion": 1}');
+  data.append(
+    'pinataMetadata',
+    `{"name": "${metadata.name}-picture", "keyvalues": {"company": "Pinata"}}`,
+  );
+
+  const config = {
+    method: 'post',
+    url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+    },
+    data: data,
+  };
+  try {
+    const uploadFileResponse = await Axios(config);
+    return `ipfs://${uploadFileResponse.data.IpfsHash}`;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const ipfsMetadataUpload = async (metadata) => {
+  const data = new FormData();
+  const metadataFile = new File(
+    [new Blob([JSON.stringify(metadata)])],
+    `${metadata.name}-metadata.json`,
+  );
+  data.append('file', metadataFile);
+  data.append('pinataOptions', '{"cidVersion": 1}');
+  data.append(
+    'pinataMetadata',
+    `{"name": "${metadata.name}-metadata", "keyvalues": {"company": "Pinata"}}`,
+  );
+
+  const config = {
+    method: 'post',
+    url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+    },
+    data: data,
+  };
+  try {
+    const metadataUploadResponse = await Axios(config);
+    return `ipfs://${metadataUploadResponse.data.IpfsHash}`;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export { ipfsImageUpload, ipfsMetadataUpload };
