536909cc880a33d93b9b2536cfe2e6cd214b740b
BeeImp <zero9231@naver.com>
2022-07-21 12:56:34 +0900

    Feat: 로그인 확인 기능

--- creator-client/pages/_app.tsx
+++ creator-client/pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { wrapper } from '../store';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { decodeJwt } from '../modules/utils';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../store/user';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const accessToken = getCookie('access_token');
+  const router = useRouter();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    try {
+      const jwtPayload = decodeJwt(accessToken?.toString() ?? '');
+      if (!jwtPayload) throw new Error();
+      if (jwtPayload?.address !== '' && jwtPayload?.username) {
+        dispatch(userActions.set_username(jwtPayload.username));
+        dispatch(userActions.set_address(jwtPayload.address));
+        dispatch(userActions.set_isAuth(true));
+      }
+    } catch (err) {
+      router.push('/auth/signin');
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
 
--- creator-client/pages/auth/signin.tsx
+++ creator-client/pages/auth/signin.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import { KlipLoginBtn } from '../../components/klip/KlipLoginBtn';
+import { getCookie } from 'cookies-next';
+import { decodeJwt } from '../../modules/utils';
+import { useRouter } from 'next/router';
 
 const AuthSignInPage: NextPage = () => {
-  return <div>SignIn</div>;
+  const accessToken = getCookie('access_token');
+  const router = useRouter();
+  useEffect(() => {
+    const jwtPayload = decodeJwt(accessToken?.toString() ?? '');
+    if (jwtPayload?.address !== '' && jwtPayload?.username) {
+      router.push('/');
+    }
+  }, []);
+  return (
+    <div>
+      SignIn
+      <KlipLoginBtn></KlipLoginBtn>
+    </div>
+  );
 };
 
 export default AuthSignInPage;
