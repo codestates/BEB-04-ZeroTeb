15b919e2079b6edd390a5da7a2cfaf97c83d24a2
BeeImp <zero9231@naver.com>
2022-07-21 13:00:28 +0900

    Fix: cors 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', '*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   await app.listen(8080, () => {
     console.log('Server Start!');
