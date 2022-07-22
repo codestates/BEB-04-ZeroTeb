03e44fe32547fdee9e48bda814d433e62a687131
BeeImp <zero9231@naver.com>
2022-07-23 00:07:30 +0900

    Fix: cors 오류 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', '*'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type'],
     preflightContinue: false,
