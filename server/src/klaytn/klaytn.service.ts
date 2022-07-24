199a0766db3403c1de3dd4dc752cd07e5239abc7
BeeImp <zero9231@naver.com>
2022-07-24 21:04:51 +0900

    Feat: KAS RPC URL로 수정

--- server/src/klaytn/klaytn.service.ts
+++ server/src/klaytn/klaytn.service.ts
@@ -363,12 +363,14 @@ export class KlaytnService {
   async test() {
     // console.log('Event Test :');
     // await this.contract.events
-    //   .Transfer()
+    //   .allEvents()
+    //   .on('connected', function (subscriptionId) {
+    //     console.log(subscriptionId);
+    //   })
     //   .on('data', function (event) {
     //     console.log(111);
     //     console.log(event); // same results as the optional callback above
     //   })
     //   .on('error', console.error);
-    // console.log(await this.contract.events.Transfer());
   }
 }
