7d1dd9b3533b4ecba5bd1929808bde78fe9a6360
BeeImp <zero9231@naver.com>
2022-07-26 03:47:31 +0900

    Feat: 리액트 스크롤 모션 가져오기

--- creator-client/pages/index.tsx
+++ creator-client/pages/index.tsx
@@ -1,7 +1,39 @@
 import type { NextPage } from 'next';
+import Wrapper from '../components/wrapper';
+import Header from '../layouts/header';
+import dynamic from 'next/dynamic';
+import Progress from '../components/Progress';
+const Animator = dynamic(
+  import('react-scroll-motion').then((it) => it.Animator as ComponentType<never>),
+  { ssr: false, loading: () => <Progress></Progress> }
+);
+
+import {
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  FadeOut,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  StickyOut,
+  Zoom,
+  ZoomIn,
+  ZoomOut,
+} from 'react-scroll-motion';
+import { ComponentType } from 'react';
 
 const Home: NextPage = () => {
-  return <div>Creator Client</div>;
+  return (
+    <Wrapper>
+      <h1>Creator Client</h1>
+      <Header></Header>
+    </Wrapper>
+  );
 };
 
 export default Home;
