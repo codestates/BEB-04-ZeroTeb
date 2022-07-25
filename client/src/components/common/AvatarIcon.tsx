43e9ed90a39a7d34c584688528579a39613afbcd
cochae <100351462+chee9835@users.noreply.github.com>
2022-07-25 15:51:58 +0900

    Fix: avartarIcon title outline css

--- client/src/components/common/AvatarIcon.tsx
+++ client/src/components/common/AvatarIcon.tsx
@@ -1,6 +1,71 @@
 import * as React from 'react'
 import { moderateScale } from 'react-native-size-matters'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { Children, cloneElement, isValidElement } from 'react'
+
+type Props = {
+  children: any
+  color: string
+  stroke: number
+}
+const styles = StyleSheet.create({
+  outline: {
+    position: 'absolute',
+  },
+})
+
+class TextStroke extends React.Component<Props> {
+  createClones = (w: number, h: number, color?: string) => {
+    const { children } = this.props
+    return Children.map(children, child => {
+      if (isValidElement(child)) {
+        const currentProps = child.props as any
+        const currentStyle = currentProps ? currentProps.style || {} : {}
+
+        const newProps = {
+          ...currentProps,
+          style: {
+            ...currentStyle,
+            textShadowOffset: {
+              width: w,
+              height: h,
+            },
+            textShadowColor: color,
+            textShadowRadius: 1,
+          },
+        }
+        return cloneElement(child, newProps)
+      }
+      return child
+    })
+  }
+
+  render() {
+    const { color, stroke, children } = this.props
+    const strokeW = stroke
+    const top = this.createClones(0, -strokeW * 1.2, color)
+    const topLeft = this.createClones(-strokeW, -strokeW, color)
+    const topRight = this.createClones(strokeW, -strokeW, color)
+    const right = this.createClones(strokeW, 0, color)
+    const bottom = this.createClones(0, strokeW, color)
+    const bottomLeft = this.createClones(-strokeW, strokeW, color)
+    const bottomRight = this.createClones(strokeW, strokeW, color)
+    const left = this.createClones(-strokeW * 1.2, 0, color)
+
+    return (
+      <View>
+        <View style={styles.outline}>{left}</View>
+        <View style={styles.outline}>{right}</View>
+        <View style={styles.outline}>{bottom}</View>
+        <View style={styles.outline}>{top}</View>
+        <View style={styles.outline}>{topLeft}</View>
+        <View style={styles.outline}>{topRight}</View>
+        <View style={styles.outline}>{bottomLeft}</View>
+        {bottomRight}
+      </View>
+    )
+  }
+}
 interface avatarProps {
   size: number
   color: string
@@ -20,7 +85,15 @@ const AvatarIcon: React.FC<avatarProps> = ({ size, color, title }) => {
           backgroundColor: color || 'skyblue',
         }}
       >
-        <Text style={{ fontSize: moderateScale(size / 2 || 64) }}>{title}</Text>
+        <TextStroke stroke={0.1} color={'#ffffff'}>
+          <Text
+            style={{
+              fontSize: moderateScale(size / 2 || 64),
+            }}
+          >
+            {title}
+          </Text>
+        </TextStroke>
       </View>
     </>
   )
