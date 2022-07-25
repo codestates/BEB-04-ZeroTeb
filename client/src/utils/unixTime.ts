24e7e69ba023483f92e63b1a443e8d25c3eca6f7
cochae <100351462+chee9835@users.noreply.github.com>
2022-07-25 19:52:36 +0900

    Fix: delete min and sec

--- client/src/utils/unixTime.ts
+++ client/src/utils/unixTime.ts
@@ -12,8 +12,6 @@ export const getDateAndTime = (uTime: number) => {
   const month = '0' + (date.getMonth() + 1)
   const day = '0' + date.getDate()
   const hour = '0' + date.getHours()
-  const minute = '0' + date.getMinutes()
-  const second = '0' + date.getSeconds()
   return (
     year +
     '-' +
@@ -22,16 +20,12 @@ export const getDateAndTime = (uTime: number) => {
     day.substr(-2) +
     ' ' +
     hour.substr(-2) +
-    ':' +
-    minute.substr(-2) +
-    ':' +
-    second.substr(-2)
+    ':00'
   )
 }
 
 export const getTime = (uTime: number) => {
   const date: Date = new Date(uTime * 1000)
   const hour = '' + date.getHours()
-  const minute = '0' + date.getMinutes()
-  return hour + ' : ' + '00'
+  return hour + ' : 00'
 }
