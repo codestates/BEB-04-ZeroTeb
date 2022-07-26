7b3ffec0fa9ebc81f77196be52c7b657160b6f61
BeeImp <zero9231@naver.com>
2022-07-27 04:10:01 +0900

    Style: EventCreateWrapper cover, wrapper 스타일 적용

--- creator-client/components/event/create/wrapper.tsx
+++ creator-client/components/event/create/wrapper.tsx
@@ -6,19 +6,31 @@ interface EventCreateWrapperProps {
 }
 
 const EventCreateWrapper: FunctionComponent<EventCreateWrapperProps> = ({ children }) => {
-  const wrapperStyle = css`
+  const coverStyle = css`
     position: relative;
     height: calc(90vh - 100px);
+
+    width: 100%;
+
+    min-width: 400px;
+    min-height: 800px;
+
+    max-width: 800px;
+
     background-color: rgba(255, 255, 255, 0.9);
     border-radius: 1em;
     padding: 2em;
 
-    overflow: hidden;
+    overflow-y: auto;
+  `;
+
+  const wrapperStyle = css`
+    overflow-y: auto;
   `;
 
   return (
-    <Box css={wrapperStyle}>
-      <div>{children}</div>
+    <Box css={coverStyle}>
+      <div css={wrapperStyle}>{children}</div>
     </Box>
   );
 };
