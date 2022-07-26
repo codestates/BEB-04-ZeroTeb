f3483de25883e73411b8307dbdc0ff38f1c93360
BeeImp <zero9231@naver.com>
2022-07-27 04:07:50 +0900

    Feat: 클래스 입력 구현

---
+++ creator-client/components/event/create/prices.tsx
@@ -0,0 +1,194 @@
+import { css, TextField } from '@mui/material';
+import { ChangeEvent, ChangeEventHandler, FunctionComponent, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { createEventActions } from '../../../store/event/createSlice';
+import EventCreateItemWrapper from './item-wrapper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+
+interface EventCreatePricesProps {}
+
+const EventCreatePrices: FunctionComponent<EventCreatePricesProps> = () => {
+  const [eventClasses, setEventClasses] = useState<string[]>(['']);
+  const dispatch = useDispatch();
+  const createEventState = useSelector((state: RootState) => state.createEvent);
+  const price = createEventState.price;
+  const propertiesHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number
+  ) => {
+    const value = Number(e.target.value);
+    switch (e.target.name) {
+      case 'price':
+        dispatch(
+          createEventActions.set_price([
+            ...price.slice(0, index),
+            {
+              ...price[index],
+              class: index,
+              [e.target.name]: value > 0 ? value : createEventState.type === 'sale' ? 1 : 0,
+            },
+            ...price.slice(index + 1),
+          ])
+        );
+        break;
+      case 'count':
+        dispatch(
+          createEventActions.set_price([
+            ...price.slice(0, index),
+            {
+              ...price[index],
+              class: index,
+              [e.target.name]: value > 0 ? value : createEventState.type === 'sale' ? 1 : 0,
+            },
+            ...price.slice(index + 1),
+          ])
+        );
+        break;
+      default:
+    }
+  };
+
+  const addPropertiesHandler = () => {
+    dispatch(
+      createEventActions.set_price([
+        ...price,
+        {
+          class: price.length,
+          count: 1,
+          price: 1,
+        },
+      ])
+    );
+    setEventClasses((state) => [...state, '']);
+  };
+
+  const removePropertiesHandler = (
+    e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    index: number
+  ) => {
+    const removeEventClasses = eventClasses.filter((item, itemIndex) => index !== itemIndex);
+    if (removeEventClasses.length > 0) {
+      setEventClasses(() => removeEventClasses);
+      const removeProperties = price.filter((item, itemIndex) => index !== itemIndex);
+      dispatch(createEventActions.set_price(removeProperties));
+    }
+  };
+
+  const wrapperStyle = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const classListStyle = css`
+    list-style: none;
+    > li {
+      display: flex;
+      justify-content: flex-start;
+      align-content: center;
+      width: 100%;
+    }
+  `;
+
+  const inputStyle = css`
+    width: 28%;
+    margin: 1%;
+  `;
+  const removeButtonStyle = css`
+    width: 10%;
+    border: none;
+    background: none;
+  `;
+  const removeIconStyle = css`
+    font-size: 2em;
+    opacity: 0.5;
+    :hover {
+      opacity: 1;
+    }
+  `;
+
+  const addButtonWrapperStyle = css`
+    width: 100%;
+  `;
+
+  const addButtonStyle = css`
+    width: 100%;
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
+    padding: 0 1em;
+    :hover {
+      transition-duration: 0.3s;
+      background-color: rgba(255, 255, 255, 0.5);
+    }
+  `;
+  return (
+    <EventCreateItemWrapper title={`클래스 입력`}>
+      <div css={wrapperStyle}>
+        <ul css={classListStyle}>
+          {eventClasses.map((eventClass, index) => {
+            return (
+              <li key={index}>
+                <TextField
+                  css={inputStyle}
+                  label={'class'}
+                  name={'class_name'}
+                  placeholder={'ex) A, B, C, ...'}
+                  onChange={(e) => {
+                    setEventClasses((state) => [
+                      ...state.slice(0, index),
+                      e.target.value,
+                      ...state.slice(index + 1),
+                    ]);
+                  }}
+                  value={eventClass}
+                ></TextField>
+                <TextField
+                  css={inputStyle}
+                  label={'가격(KLAY)'}
+                  name={'price'}
+                  type={'number'}
+                  // placeholder={'Value..'}
+                  onChange={(e) => {
+                    propertiesHandler(e, index);
+                  }}
+                  value={String(price[index].price)}
+                ></TextField>
+                <TextField
+                  css={inputStyle}
+                  label={'인원수'}
+                  name={'count'}
+                  type={'number'}
+                  // placeholder={'Value..'}
+                  onChange={(e) => {
+                    propertiesHandler(e, index);
+                  }}
+                  value={String(price[index].count)}
+                ></TextField>
+                {index > 0 ? (
+                  <button
+                    css={removeButtonStyle}
+                    onClick={(e) => {
+                      removePropertiesHandler(e, index);
+                    }}
+                  >
+                    <DeleteIcon css={removeIconStyle}></DeleteIcon>
+                  </button>
+                ) : undefined}
+              </li>
+            );
+          })}
+        </ul>
+        <div css={addButtonWrapperStyle}>
+          <button css={addButtonStyle} onClick={addPropertiesHandler}>
+            <AddIcon></AddIcon>
+          </button>
+        </div>
+      </div>
+    </EventCreateItemWrapper>
+  );
+};
+
+export default EventCreatePrices;
